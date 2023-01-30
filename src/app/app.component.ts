07329786e20ae69c1909331761942e0c61efb8bc
ColinHRL <colin@hiddenriverlabs.net>
2023-01-29 22:10:23 -0700

    side nav wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, ViewChild } from '@angular/core';
+import { MatDrawerToggleResult, MatSidenav } from '@angular/material/sidenav';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'safe-money';
+export class AppComponent implements OnDestroy {
+  showFiller = false;
+  handPreference: 'start' | 'end' = 'start';
+  mobileQuery: MediaQueryList;
+  fillerNav = Array.from({ length: 5 }, (_, i) => `Nav Item ${i + 1}`);
+  private _mobileQueryListener: () => void;
+  @ViewChild('snav') drawer?: MatSidenav;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+  toggleHandPreference() {
+    if (this.handPreference === 'start') {
+      this.handPreference = 'end';
+    } else {
+      this.handPreference = 'start';
+    }
+    this.drawer?.close().then((toggleResult: MatDrawerToggleResult) => {
+      console.log(toggleResult);
+      // if (this.handPreference === 'start') {
+      //   this.handPreference = 'end';
+      // } else {
+      //   this.handPreference = 'start';
+      // }
+    }, (rejectedMsg: any) => {
+      console.log(rejectedMsg);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -14,6 +19,11 @@ import { ServiceWorkerModule } from '@angular/service-worker';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatListModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: !isDevMode(),
       // Register the ServiceWorker as soon as the application is stable
